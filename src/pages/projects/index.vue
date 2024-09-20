9813dabbe64f9685c8f94e09ac4e904f79cf26fd
SValac <shiro.valac@gmail.com>
2024-09-20 03:05:29 -0600

    moving fetching projecto from home to projects

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+/* 
+  IIFE = Immediately Invoke function Expression
+  A JavaScript function that runs as soon as it is defined. Also known as an IIFE.
+*/
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) {
+    console.log(error)
+  }
+  console.log('projects ', data)
+})()
+</script>
 
 <template>
   <div>
