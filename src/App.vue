618f1da5a6fa5df012b58cf53b528637db5868c7
SValac <shiro.valac@gmail.com>
2024-09-20 13:03:43 -0600

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
+import Input from './components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
-console.log(supabase)
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 </script>
 
 <template>
+  <nav class="flex justify-between items-center px-6 h-16 border-b bg-muted/40">
+    <form class="w-full max-w-96">
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
+    </form>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
   <main>
     <router-view />
   </main>
