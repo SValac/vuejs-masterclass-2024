cdbfc2649a7691c77623945ff12492fc51d46264
SValac <shiro.valac@gmail.com>
2024-09-20 00:14:27 -0600

    added seed command

--- database/seed.js
+++ database/seed.js
@@ -3,4 +3,5 @@ import { fakerES_MX } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_KEY)
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVIVE_ROLE_KEY)
+console.log(supabase)
