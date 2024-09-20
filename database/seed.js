e5c947e7b3ed78a65c57021506a336fe55f4a86d
SValac <shiro.valac@gmail.com>
2024-09-19 21:44:35 -0600

    created migration

---
+++ database/seed.js
@@ -0,0 +1,6 @@
+/* eslint-env node*/
+import { fakerES_MX } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_KEY)
