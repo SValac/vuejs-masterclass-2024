f08e99e43807e948767dccbb6cd71d5ff7537885
SValac <shiro.valac@gmail.com>
2024-09-20 02:45:26 -0600

    bulk seed

--- database/seed.js
+++ database/seed.js
@@ -1,7 +1,23 @@
 /* eslint-env node*/
-import { fakerES_MX } from '@faker-js/faker'
+import { fakerES_MX as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVIVE_ROLE_KEY)
-console.log(supabase)
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+const seedProject = async (numEntries) => {
+  const projects = []
+  for (let i = 1; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+    projects.push({
+      name,
+      slug: name.toLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['todo', 'in-progress', 'done']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+  const { data, error, status } = await supabase.from('projects').insert(projects)
+  console.log(data, status, error)
+}
+
+await seedProject(10)
