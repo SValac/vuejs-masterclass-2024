50567a8fb6313e6355d2c0891cef0417307f26b3
SValac <shiro.valac@gmail.com>
2024-09-23 16:23:25 -0600

    update custom query type

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,20 +3,23 @@ import { supabase } from '@/lib/supabaseClient'
 import type { Tables } from 'database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
+import type { QueryData } from '@supabase/supabase-js'
 
 usePageStore().pageData.title = 'My Tasks'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null)
-
-const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select(`
-  *,
-  projects(
-    id,
-    name,
-    slug
+const tasksWithProjectsQuery = supabase.from('tasks').select(`
+*,
+projects(
+  id,
+  name,
+  slug
   )
   `)
+
+type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
+const tasks = ref<TasksWithProjects | null>(null)
+const getTasks = async () => {
+  const { data, error } = await tasksWithProjectsQuery
   if (error) {
     console.log(error)
   }
@@ -26,7 +29,7 @@ const getTasks = async () => {
 }
 await getTasks()
 
-const columns: ColumnDef<Tables<'tasks'>>[] = [
+const columns: ColumnDef<TasksWithProjects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -59,15 +62,16 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.projects.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('projects').name
-      )
-      // return h('div', { class: 'text-left font-medium' }, row.getValue('projects').name)
+      return row.original.projects
+        ? h(
+            RouterLink,
+            {
+              to: `/projects/${row.original.projects.slug}`,
+              class: 'text-left font-medium hover:bg-muted block w-full'
+            },
+            () => row.original.projects?.name
+          )
+        : ''
     }
   },
   {
