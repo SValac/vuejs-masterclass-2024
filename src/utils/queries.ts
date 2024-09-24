6e73aa1a5909c9b57c253776511843a703c94f5c
SValac <shiro.valac@gmail.com>
2024-09-23 20:59:52 -0600

    set task page dynamic

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -12,6 +12,24 @@ export const tasksWithProjectsQuery = supabase.from('tasks').select(`
 
 export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
+export const taskQuery = (id: string) =>
+  supabase
+    .from('tasks')
+    .select(
+      `
+  *,
+  projects(
+    id,
+    name,
+    slug
+  )
+  `
+    )
+    .eq('id', id)
+    .single()
+
+export type Task = QueryData<ReturnType<typeof taskQuery>>
+
 export const projectsQuery = supabase.from('projects').select()
 export type ProjectsQuery = QueryData<typeof projectsQuery>
 
